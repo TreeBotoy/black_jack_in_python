45cb04c9fb2f673cae90132d50a4340c12194298
botoy <botoydaniel@gmail.com>
2026-03-05 15:11:05 +0800

    added frames for player and dealer and also imputed a mainloop so that the GUI works

--- black_jack.py
+++ black_jack.py
@@ -1,14 +1,32 @@
 #imports
-import tkinter
-import random
-from PIL import Image, ImageTk
+from tkinter import *
 
-root = tkinter.Tk()
+root = Tk()
 root.title("Black Jack Game")
-root.iconbitmap("")
-root .geometry("600x400")
-root.config(bg="green")
+root.geometry("900x500")
+root.configure(background="green")
 
+my_frame = Frame(root, bg="green")
+my_frame.pack(pady=20)
+
+dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
+dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
+
+player_frame = LabelFrame(my_frame, text="Player", bd=0)
+player_frame.grid(row=0, column=1, ipadx=20)
+
+dealer_frame = Label(dealer_frame, text="")
+dealer_frame.pack(pady=20)
+
+player_frame = Label(player_frame, text="")
+player_frame.pack(pady=20)
+
+#Buttons
+shuffle_button = Button(root, text="Shuffle Deck", font=("Helvetica", 14), bg="white", fg="black")
+shuffle_button.pack(pady=20)
+
+hit_button = Button(root, text="Hit", font=("Helvetica", 14), bg="white", fg="black")
+hit_button.pack(pady=20)
 
 #cards
 
@@ -20,3 +38,4 @@ root.config(bg="green")
 
 #card shuffling
 
+root.mainloop()
