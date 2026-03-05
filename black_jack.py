b44839f8994f0b1d7c3dacf44389c26071be1cca
botoy <botoydaniel@gmail.com>
2026-03-05 12:25:55 +0800

    adding root lines for Tk GUI

--- black_jack.py
+++ black_jack.py
@@ -3,6 +3,13 @@ import tkinter
 import random
 from PIL import Image, ImageTk
 
+root = tkinter.Tk()
+root.title("Black Jack Game")
+root.iconbitmap("")
+root .geometry("600x400")
+root.config(bg="green")
+
+
 #cards
 
 
