3f68d1aa89413cf3466edad481e128d924755c0f
botoy <botoydaniel@gmail.com>
2026-03-05 15:31:14 +0800

    added some card shuffle and some dealer and player labels

--- black_jack.py
+++ black_jack.py
@@ -1,10 +1,36 @@
 #imports
 from tkinter import *
+import random
 
 root = Tk()
 root.title("Black Jack Game")
 root.geometry("900x500")
 root.configure(background="green")
+
+#card shuffling
+def shuffle():
+    card_suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    card_values = range(2, 15)
+
+    global deck
+    deck = []
+    for suit in card_suits:
+        for value in card_values:
+            deck.append(f"{value} of {suit}")
+    
+    global player, dealer
+    dealer = []
+    player = []
+
+    card=random.choice(deck)
+    deck.remove(card)
+    dealer.append(card)
+    dealer_label.config(text=card)
+
+    card=random.choice(deck)
+    deck.remove(card)
+    player.append(card)
+    player_label.config(text=card)
 
 my_frame = Frame(root, bg="green")
 my_frame.pack(pady=20)
@@ -21,8 +47,14 @@ dealer_frame.pack(pady=20)
 player_frame = Label(player_frame, text="")
 player_frame.pack(pady=20)
 
+dealer_label = Label(dealer_frame, text="")
+dealer_label.pack(pady=20)
+
+player_label = Label(player_frame, text="")
+player_label.pack(pady=20)
+
 #Buttons
-shuffle_button = Button(root, text="Shuffle Deck", font=("Helvetica", 14), bg="white", fg="black")
+shuffle_button = Button(root, text="Shuffle Deck", font=("Helvetica", 14), bg="white", fg="black", command=shuffle)
 shuffle_button.pack(pady=20)
 
 hit_button = Button(root, text="Hit", font=("Helvetica", 14), bg="white", fg="black")
@@ -35,7 +67,5 @@ hit_button.pack(pady=20)
 #stand
 
 #hit
-
-#card shuffling
 
 root.mainloop()
